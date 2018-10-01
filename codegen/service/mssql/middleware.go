6bf0961cdfa309f80d941706348f62cd9d7c72bb
Denis Shumkov <DShumkov@nefco.ru>
2018-10-01 17:14:27 +0300

    Add mssql context and middleware

---
+++ codegen/service/mssql/middleware.go
@@ -0,0 +1,81 @@
+package mssql
+
+import (
+	"context"
+
+	"github.com/jmoiron/sqlx"
+
+	"github.com/99designs/gqlgen/graphql"
+)
+
+type SQLContext struct {
+	db *sqlx.DB
+	tx *sqlx.Tx
+}
+
+func NewContext(db *sqlx.DB) *SQLContext {
+	return &SQLContext{db: db}
+}
+
+func (c *SQLContext) IsTx() bool {
+	return c.tx != nil
+}
+
+func (c *SQLContext) Begin() (*sqlx.Tx, error) {
+	if c.IsTx() {
+		return c.tx, nil
+	}
+	tx, err := c.db.Beginx()
+	if err != nil {
+		return nil, err
+	}
+	c.tx = tx
+	return tx, nil
+}
+
+func (c *SQLContext) Commit() error {
+	if !c.IsTx() {
+		return nil
+	}
+	if err := c.tx.Commit(); err != nil {
+		return err
+	}
+	c.tx = nil
+	return nil
+}
+
+type key int
+
+const ctxKey key = 1
+
+func withContext(ctx context.Context, sqlCtx *SQLContext) context.Context {
+	return context.WithValue(ctx, ctxKey, sqlCtx)
+}
+
+func getContext(ctx context.Context) *SQLContext {
+	val := ctx.Value(ctxKey)
+	if val == nil {
+		return nil
+	}
+	return val.(*SQLContext)
+}
+
+func Begin(ctx context.Context) (*sqlx.Tx, error) {
+	return getContext(ctx).Begin()
+}
+
+func middleware(db *sqlx.DB) graphql.RequestMiddleware {
+	return func(ctx context.Context, next func(ctx context.Context) []byte) []byte {
+		sqlCtx := NewContext(db)
+
+		res := next(withContext(ctx, sqlCtx))
+
+		if sqlCtx.IsTx() {
+			if err := sqlCtx.Commit(); err != nil {
+				panic(err)
+			}
+		}
+
+		return res
+	}
+}
--- codegen/service/mssql/mssql.go
+++ codegen/service/mssql/mssql.go
@@ -2,12 +2,13 @@ package mssql
 
 import (
 	"bytes"
-	"context"
 	"gitlab/nefco/platform/codegen/schema"
 	"gitlab/nefco/platform/codegen/template"
 
-	"github.com/99designs/gqlgen/graphql"
+	"github.com/jmoiron/sqlx"
+
 	"github.com/99designs/gqlgen/handler"
+	_ "github.com/denisenkom/go-mssqldb"
 	"github.com/gobuffalo/packr"
 	"github.com/spf13/viper"
 )
@@ -23,17 +24,15 @@ func (s *mssql) Name() string {
 }
 
 func (s *mssql) Init(v *viper.Viper) (handler.Option, error) {
-	return handler.RequestMiddleware(middleware()), nil
+	db, err := sqlx.Connect("", "")
+	if err != nil {
+		return nil, err
+	}
+	return handler.RequestMiddleware(middleware(db)), nil
 }
 
 func (s *mssql) Generate(a *schema.Action) (string, error) {
 	return generate(a)
-}
-
-func middleware() graphql.RequestMiddleware {
-	return func(ctx context.Context, next func(ctx context.Context) []byte) []byte {
-		return next(ctx)
-	}
 }
 
 func generate(a *schema.Action) (string, error) {
@@ -52,3 +51,66 @@ func generate(a *schema.Action) (string, error) {
 
 	return buff.String(), nil
 }
+
+/*
+func DBMiddleware(db *DB) graphql.RequestMiddleware {
+	return func(ctx context.Context, next func(ctx context.Context) []byte) []byte {
+		reqCtx := graphql.GetRequestContext(ctx)
+
+		fmt.Println("RequestMiddleware", reqCtx.Doc.Operations)
+
+		dbCtx := NewDBContext(db)
+
+		ctx = WithDBContext(ctx, dbCtx)
+
+		res := next(ctx)
+
+		fmt.Println("Result", *dbCtx.tx)
+
+		return res
+	}
+}
+
+type DB struct {
+	Connection string
+}
+
+func NewDB() *DB {
+	return &DB{
+		Connection: "connection",
+	}
+}
+
+type platformContextKey int
+
+const dbContextKey platformContextKey = 1
+
+type DBContext struct {
+	DB *DB
+	tx *string
+}
+
+func NewDBContext(db *DB) *DBContext {
+	return &DBContext{
+		DB: db,
+	}
+}
+
+func (c *DBContext) Tx(s string) {
+	if c.tx == nil {
+		c.tx = &s
+	}
+}
+
+func WithDBContext(ctx context.Context, db *DBContext) context.Context {
+	return context.WithValue(ctx, dbContextKey, db)
+}
+
+func GetDBContext(ctx context.Context) *DBContext {
+	val := ctx.Value(dbContextKey)
+	if val == nil {
+		return nil
+	}
+	return val.(*DBContext)
+}
+*/
