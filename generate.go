186afcda7ae654a7edf202c3a276cf202e4463b1
Denis Shumkov <DShumkov@nefco.ru>
2018-10-02 11:41:11 +0300

    Fix generate.go

--- generate.go
+++ generate.go
@@ -1,6 +1,5 @@
 // go:generate gorunpkg github.com/gobuffalo/packr/packr
-// go:generate gorunpkg gitlab/nefco/platform/platform codegen --schema
+//go:generate gorunpkg gitlab/nefco/platform/platform codegen
 //go:generate gorunpkg github.com/99designs/gqlgen
-// go:generate gorunpkg gitlab/nefco/platform/platform codegen --service
 
 package platform
