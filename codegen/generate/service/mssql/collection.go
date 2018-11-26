215b13343ee9b6742b4026d6d37bb9e2c5911527
RShamilov <RShamilov@nefco.ru>
2018-11-26 12:00:39 +0300

    IN/not IN in collection

--- codegen/generate/service/mssql/collection.go
+++ codegen/generate/service/mssql/collection.go
@@ -2,7 +2,6 @@ package mssql
 
 import (
 	"context"
-	"fmt"
 	"github.com/jmoiron/sqlx"
 	"gitlab/nefco/platform/codegen/generate/service/mssql/build"
 	"gitlab/nefco/platform/codegen/generate/service/mssql/query"
@@ -38,38 +37,17 @@ func Collection(ctx context.Context, result interface{}) error {
 		return err
 	}
 
-	/*stmt, err := tx.PrepareNamed(query.Query())
-	if err != nil {
-		return err
-	}*/
-
 	_query, args, err := sqlx.Named(query.Query(), query.Arg())
 	if err != nil {
 		return err
 	}
+
 	_query, args, err = sqlx.In(_query, args...)
 	if err != nil {
 		return err
 	}
-
-	rows, err := tx.NamedQuery(query.Query(), query.Arg())
-	if err != nil {
-		return err
-	}
-	for rows.Next() {
-		var id int
-		err = rows.Scan(&id)
-		fmt.Println("id=",id)
-	}
 	_query = tx.Rebind(_query)
-	fmt.Println("--------------")
-	fmt.Println(_query)
-
-	stmt, err := tx.PrepareNamed(_query)
-	if err != nil {
-		return err
-	}
-	if err := stmt.Select(result, args); err != nil {
+	if err := tx.Select(result, _query, args...); err != nil {
 		return err
 	}
 
